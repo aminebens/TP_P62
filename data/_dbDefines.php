a09ceaf9f67369abdd28b61a02cbdf4bbb9e8e6b
Amine <aminebenasla@gmail.com>
2015-09-06 14:13:07 -0400

    liste des constantes pour la DB

---
+++ data/_dbDefines.php
@@ -0,0 +1,9 @@
+<?php
+// Constantes pour la connexion à la base de données
+define('DB_HOST', '127.0.0.1');
+define('DB_USER', 'root');
+define('DB_PASS', 'dbrootpass');
+define('DB_NAME', 'comicsdb');
+define('DB_PORT', 3306);
+
+define('TB_CATEGORIES', 'categories');
