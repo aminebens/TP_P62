04af231badfb02afab030c5d06b8b73589fad715
Amine <aminebenasla@gmail.com>
2015-09-07 18:53:32 -0400

    filtre

--- views/_view_filter.php
+++ views/_view_filter.php
@@ -7,9 +7,10 @@ define('SUBMIT', 'submit');
 $genres = get_genres();
 
 if (array_key_exists(SUBMIT, $_GET)) {
-    if (isset($_GET[P_GENRES])) {
+    if (isset($_GET[P_GENRES]) && $_GET[P_GENRES] != '-1') {
         $comics = comics_genres($_GET[P_GENRES][0]);
-        //var_dump($_GET);
+    } else {
+        $comics = get_comics();
     }
 }
 ?>
@@ -18,7 +19,9 @@ if (array_key_exists(SUBMIT, $_GET)) {
     <select id="genres_list" name="<?php echo P_GENRES; ?>" class="form-control" >
         <option value="-1">choisir ...</option>
         <?php foreach ($genres as $genre) { ?>
-            <option value="<?php echo $genre[GENRE_ID] ?>"><?php echo $genre[GENRE_NAME] ?></option>
+            <option value="<?php echo $genre[GENRE_ID] ?>" <?php
+            echo ( array_key_exists(P_GENRES, $_GET) && ($genre[GENRE_ID] == $_GET[P_GENRES]) ) ? 'selected=selected' : '';
+            ?>><?php echo $genre[GENRE_NAME] ?></option>
         <?php } ?>
     </select>
     <input class="btn btn-info" type="submit" name="<?php echo SUBMIT; ?>" value="Soumettre" />
