54cedf7eac6d5fcee6795edde50a7bae8f6e5743
Nathan_Bernard <nathanbrnrd@gmail.com>
2015-09-07 16:05:55 -0400

    page du panier d'ahat

---
+++ cart.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo SITE_NAME; ?></title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="style/main.css" />
+</head>
+<body>
+<?php require_once('views/_view_header.php') ?>
+
+
+
+
+<?php require_once('views/_view_footer.php') ?>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<?php $mysqli->close(); ?>
+</body>
+</html>
