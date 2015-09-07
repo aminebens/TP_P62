9415eb9e54cbc23d12bce7388b96f10113ad5efc
Nathan_Bernard <nathanbrnrd@gmail.com>
2015-09-07 19:10:20 -0400

    pour pull!!!

--- views/_view_filter.php
+++ views/_view_filter.php
@@ -16,18 +16,18 @@ if (array_key_exists(SUBMIT, $_GET)) {
 
 
 <!--Genre && search form-->
-<form class="form-inline" action="<?php echo PAGE_INDEX ?>">
+    <form class="form-inline" action="<?php echo PAGE_INDEX ?>">
 
-    <div class="form-group">
-        <input type="text" name="<?php echo P_INPUT_SEARCH; ?>" class="form-control" placeholder="Search" />
-    </div>
-    <input type="submit" name="<?php echo NAV_SEARCH; ?>" class="btn btn-default" value="Rechercher" />
-    <label for="genres_list">Genres:</label>
-    <select id="genres_list" name="<?php echo P_GENRES; ?>" class="form-control" >
-        <option value="-1">choisir ...</option>
-        <?php foreach ($genres as $genre) { ?>
-            <option value="<?php echo $genre[GENRE_ID] ?>"><?php echo $genre[GENRE_NAME] ?></option>
-        <?php } ?>
-    </select>
-    <input class="btn btn-info" type="submit" name="<?php echo SUBMIT; ?>" value="Soumettre" />
-</form>
+        <div class="form-group">
+            <input type="text" name="<?php echo P_INPUT_SEARCH; ?>" class="form-control" placeholder="Search" />
+        </div>
+        <input type="submit" name="<?php echo NAV_SEARCH; ?>" class="btn btn-default" value="Rechercher" />
+        <label for="genres_list">Genres:</label>
+        <select id="genres_list" name="<?php echo P_GENRES; ?>" class="form-control" >
+            <option value="-1">choisir ...</option>
+            <?php foreach ($genres as $genre) { ?>
+                <option value="<?php echo $genre[GENRE_ID] ?>"><?php echo $genre[GENRE_NAME] ?></option>
+            <?php } ?>
+        </select>
+        <input class="btn btn-info" type="submit" name="<?php echo SUBMIT; ?>" value="Soumettre" />
+    </form>
