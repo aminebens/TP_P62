335b1c8960d651a7d5f50864a17426bbcc2c7113
Amine <aminebenasla@gmail.com>
2015-09-08 19:23:41 -0400

    login modal functionality

--- views/_view_header.php
+++ views/_view_header.php
@@ -30,10 +30,12 @@ if (array_key_exists(NAV_SEARCH, $_GET)) {
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                  <ul class="nav navbar-nav navbar-right">
                      <?php if (array_key_exists(FIRST_NAME, $_SESSION)) { ?>
+                        <!-- Logged in -->
                          <li><a href="#">Bonjour, <?php echo $_SESSION[FIRST_NAME] ?></a></li>
                          <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart lg" aria-hidden="true"></span> (0)</a></li>
                          <li><a href="#"><span class="glyphicon glyphicon-log-out lg" aria-hidden="true"></span></a></li>
                      <?php } else { ?>
+                         <!-- Logged out -->
                          <li><a href="#" data-toggle="modal" data-target="#<?php echo LOGIN ?>" >Connexion</a></li>
                          <li><a href="#" data-toggle="modal" data-target="#<?php echo SIGN_UP ?>">S'inscrire</a></li>
                      <?php } ?>
