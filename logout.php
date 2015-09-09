74af9ef7cd4cfdc995f824ebee7e4cff91fe3eff
Amine <aminebenasla@gmail.com>
2015-09-08 20:11:24 -0400

    logout

---
+++ logout.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'data/_data.php';
+session_start();
+$_SESSION = array();
+session_destroy();
+setcookie ('PHPSESSID', '', time()-3600);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo SITE_NAME; ?></title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="style/main.css" />
+</head>
+<body>
+<?php require_once('views/_view_header.php') ?>
+<?php require_once('views/_view_login.php'); ?>
+<?php require_once ('views/_view_signUp.php');?>
+<h1>Logged out!</h1>
+<p><a href="index.php">Acceuil</a></p>
+<?php require_once('views/_view_footer.php') ?>
+<!-- Latest compiled and minified JavaScript -->
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<?php $mysqli->close(); ?>
+</body>
+</html>
